b716ee937e7f4d85d2450d1bbbc9f381f6aeccd9
KanujanS <kanujansk@gmail.com>
2025-07-25 23:29:47 +0530

    Finalized

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -5,11 +5,11 @@ const Task = ({ tasks, onDone, onDelete }) => {
     <div className='w-full max-w-2xl mx-auto flex flex-col gap-5'>
       {tasks.map((t) => (
         <div key={t.id} className='flex flex-col sm:flex-row justify-between gap-4 bg-blue-100 p-4 rounded-xl shadow-md text-blue-900'>
-          <div>
+          <div className='space-y-3'>
             <p className='text-xl font-semibold'>{t.title}</p>
             <p className='text-sm text-gray-700'>{t.description}</p>
           </div>
-          <div className='flex gap-2 self-end sm:self-center'>
+          <div className='flex md:flex-col gap-2 self-end sm:self-center'>
             <button
               onClick={() => onDone(t.id)}
               className='px-3 py-1 bg-blue-900 text-white rounded hover:bg-blue-700'
